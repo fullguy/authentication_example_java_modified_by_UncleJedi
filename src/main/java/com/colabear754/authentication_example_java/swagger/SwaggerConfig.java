a015a23f3ae86184e2f9f1a25e6c95c2c319ba81
Fullguy <jedikimorg@gmail.com>
2024-08-18 09:33:50 +0900

    SwaggerConfig 수정

--- src/main/java/com/colabear754/authentication_example_java/swagger/SwaggerConfig.java
+++ src/main/java/com/colabear754/authentication_example_java/swagger/SwaggerConfig.java
@@ -17,14 +17,12 @@ public class SwaggerConfig {
     public OpenAPI swaggerApi() {
         return new OpenAPI()
                 .components(new Components()
-                        // 여기부터 추가 부분
-                        .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
-                                .name(SECURITY_SCHEME_NAME)
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")))
+                .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                        .name(SECURITY_SCHEME_NAME)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")))
                 .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
-                // 여기까지
                 .info(new Info()
                         .title("[수정판] 스프링시큐리티 + JWT 예제 (by 엉클제다이)")
                         .description("스프링시큐리티와 JWT를 이용한 사용자 인증 예제")
